2e03b1d7bde6ad453150e99248f0299b5d440a30
IvailoManolov <ivomoreras@abv.bg>
2023-05-20 13:17:31 +0300

    DB set for channels and videos

--- youtube-clone/youtube-server/index.js
+++ youtube-clone/youtube-server/index.js
@@ -1,1 +1,21 @@
 import express from "express"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const application = express()
+const connect = async () => {
+    try{
+        await mongoose.connect(process.env.CONNECTION_STRING)
+        console.log("Connected to DB")
+    }
+    catch(err){
+        console.log(err)
+    }
+}
+
+application.listen(8800,() => {
+    console.log("Listening")
+    connect()
+})
