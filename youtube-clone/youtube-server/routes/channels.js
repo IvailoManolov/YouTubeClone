d53aa5591111ef9da590750161a2a6912e81b4bc
IvailoManolov <ivomoreras@abv.bg>
2023-05-23 21:55:33 +0300

    ChannelController API template

--- youtube-clone/youtube-server/routes/channels.js
+++ youtube-clone/youtube-server/routes/channels.js
@@ -1,6 +1,24 @@
 import express from "express"
+import { dislike, getUser, like, subscribe, unsubscribe, updateUser } from "../controllers/ChannelController";
 
 const router = express.Router();
 
+// updating a user.
+router.put("/:id", updateUser);
+
+// get a user.
+router.get("/find/:id", getUser);
+
+// subscribe a user.
+router.get("/sub/:id", subscribe);
+
+// unsubscribe a user.
+router.get("/sub/:id", unsubscribe);
+
+// like a video.
+router.get("/sub/:videoId", like);
+
+// dislike a video.
+router.get("/sub/:videoId", dislike);
 
 export default router;
