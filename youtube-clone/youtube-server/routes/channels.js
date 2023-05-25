20ecf36f749117724dcca8b567e55abbed47b362
IvailoManolov <ivomoreras@abv.bg>
2023-05-25 17:22:48 +0300

    token verification integration

--- youtube-clone/youtube-server/routes/channels.js
+++ youtube-clone/youtube-server/routes/channels.js
@@ -11,15 +11,15 @@ router.put("/:id", verifyToken, updateUser);
 router.get("/find/:id", getUser);
 
 // subscribe a user.
-router.get("/sub/:id", subscribe);
+router.get("/sub/:id", verifyToken, subscribe);
 
 // unsubscribe a user.
-router.get("/sub/:id", unsubscribe);
+router.get("/sub/:id", verifyToken, unsubscribe);
 
 // like a video.
-router.get("/sub/:videoId", like);
+router.get("/sub/:videoId", verifyToken, like);
 
 // dislike a video.
-router.get("/sub/:videoId", dislike);
+router.get("/sub/:videoId", verifyToken, dislike);
 
 export default router;
