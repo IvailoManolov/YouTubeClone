de27422149d2e1b04348e69fd96d2448342e0502
IvailoManolov <ivomoreras@abv.bg>
2023-05-25 17:16:30 +0300

    updating user API

--- youtube-clone/youtube-server/controllers/ChannelController.js
+++ youtube-clone/youtube-server/controllers/ChannelController.js
@@ -1,7 +1,18 @@
-export const updateUser = (req, res, next) => {
+import Channel from "../models/Channel";
+
+export const updateUser = async (req, res, next) => {
 
     if(req.params.id === req.user.id) {
+        try {
+            const updatedChannel = await Channel.findByIdAndUpdate(req.params.id, {
+                $set : req.body
+            },{new:true});
 
+            res.status(200).json(updatedChannel);
+        }
+        catch(err) {
+            next(err)
+        }
     }
     else {
         return next(createError(403,"You can update only your account!"));
